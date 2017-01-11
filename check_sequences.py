e06afbc5aa2bd7468e9270475090f67293d29f53
Christine Jandrasits <jandrasitsc@rki.de>
2017-01-11 14:57:01 +0100

    Sanity check file to compare sequences listed in XMFA with original FASTA sequences.

---
+++ check_sequences.py
@@ -0,0 +1,48 @@
+import argparse
+import sys
+import pdb
+
+
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+
+from seqseqpan.io import Parser
+
+
+def main():
+    global args
+
+    parser = Parser()
+
+    alignment = parser.parse_xmfa(args.xmfa_f)
+
+    genome_dict = {}
+
+    for number, genome in alignment.genomes.items():
+        with open(genome.file_path, "r") as fastaf:
+            genome_dict[number] = list(SeqIO.parse(fastaf, "fasta"))[0]
+
+    #pdb.set_trace()
+
+    for lcb in alignment.lcbs:
+        for entry in lcb.entries:
+            seq = str(genome_dict[entry.genome_nr].seq[entry.start-1:entry.end])
+            entry_seq = str(entry.sequence.replace("-", ""))
+            if entry.strand == "-":
+                seq_bio = Seq(entry_seq)
+                entry_seq = str(seq_bio.reverse_complement())
+            if seq != entry_seq:
+                print("Wrong sequence: " + genome_dict[entry.genome_nr].description + "(" + str(entry.genome_nr) + "): " + str(entry.start) + "-" + str(entry.end) + " (" + str(len(entry_seq)) + "," + str(len(seq)) +") "+ str(entry.strand) + "\n" + entry_seq + "\n" + seq + "\n")
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-x", "--xmfa", dest="xmfa_f", help="XMFA input file")
+    # parser.add_argument("-p", "--output_path", dest="output_p", help="path to output directory", required=True)
+    # parser.add_argument("-n", "--name", dest="output_name", help="file prefix and sequence header for consensus FASTA / XFMA file", required=True)
+    args = parser.parse_args()
+
+    main()
