5a44f4393a3fa62fc3f1c99f265a4bc2d0c83e38
Christine Jandrasits <jandrasitsc@rki.de>
2017-01-27 14:02:50 +0100

    Update genome description script

--- genomedescription.py
+++ genomedescription.py
@@ -4,18 +4,19 @@ from Bio import SeqIO
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('infile', nargs=1)
+    parser.add_argument("-i", "--input", dest="genome_list", help= "File with list of /paths/to/files.fasta", required=True)
+    parser.add_argument("-o", "--output", dest="genome_desc_f", help="name of output file", required=True)
 
     args = parser.parse_args()
 
-    with open(args.infile[0], "r") as in_file:
+with open(args.genome_desc_f, "w") as out_file, open(args.genome_list, "r") as in_file:
 
         allfiles = in_file.readlines()
 
         for i in range(0,len(allfiles)):
 
-            fields = allfiles[i].strip().split(";")
-            with open(fields[0], "r") as fasta:
+            file = allfiles[i].strip()
+            with open(file, "r") as fasta:
                 for record in SeqIO.parse(fasta, "fasta"):
                     cur_length = len(record.seq)
-                    print("\t".join([str(i+1), record.description, str(cur_length)]))
+                    out_file.write("\t".join([str(i+1), record.description, str(cur_length)]) + "\n")
